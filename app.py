7cf5ea1a3539c2327c8ce5fb9aea41863e7ef408
David Amirkhanov <mail.websofter@gmail.com>
2024-06-04 13:08:21 +0300

    updt

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import os
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html', data={})
-
+  return render_template('index.html', data = {})
 
 @app.errorhandler(500)
-def internal_server_error(e):
-    print('yes')    
-    return 'It works!', 500
+def internal_server_error(e):  
+    return 'Server Error occured!', 500
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
--- static/main.js
+++ static/main.js
@@ -6,25 +6,60 @@ function init() {
   L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    maxZoom: 18,
+    maxZoom: 22,
   }).addTo(map);
   map.attributionControl.setPrefix(""); // Don't show the 'Powered by Leaflet' text.
 
-  var london = new L.LatLng(51.5, 0);
-  map.setView(london, 10);
+  var london = new L.LatLng(55.751244, 37.618423);
+  map.setView(london, 12);
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+  })
+
 }
 
-function popupMitText(pos, txt) {
-  var popupMitText = L.popup().setLatLng(pos).setContent(txt).openOn(map);
+// function popupWithText(pos, txt) {
+//   var popupWithText = L.popup().setLatLng(pos).setContent(txt).openOn(map);
+//   map.panTo(new L.LatLng(pos[0], pos[1]));
+// }
+
+function markerWithText(pos, txt) {
+  var markerWithText = L.marker(pos).addTo(map).on('click', function(e) {
+      $('#label-lon').val(e.latlng.lng)
+      $('#label-lat').val(e.latlng.lat)
+  })
+
+  if(txt) {
+    markerWithText.bindPopup(txt);
+    markerWithText.openPopup();    
+  }
   map.panTo(new L.LatLng(pos[0], pos[1]));
+}
+const isFloat = n => Number(n) === n && n % 1 !== 0;
+const addMarker = (input = { lon : null, lat : null }) => {
+  if(!(input.lon && input.lat)) {
+    input.lon = $('#input-lon').val()
+    input.lat = $('#input-lat').val()
+  }
+  //
+  let isOk = true
+  //
+  for (const [key, value] of Object.entries(input)) {
+    if(isFloat((1 * value))) {
+      $(`#input-${key}`).removeClass('uk-form-danger')
+      $('#text-danger').text(``)
+    } else {
+      isOk = false
+      $('#text-danger').html(`Неправильный формат поля <b>${key}</b>. Пример заполнения: lon: 37.618423, lat:55.751244 (Moscow)`)
+      $(`#input-${key}`).addClass('uk-form-danger')
+    }
+  }
+  //
+  const msg = $('#input-msg').val()
+  //
+  if(isOk) markerWithText([input.lat, input.lon], msg)
 }
 
-function markerMitText(pos, txt) {
-  var markerMitText = L.marker(pos).addTo(map);
-  markerMitText.bindPopup(txt);
-  markerMitText.openPopup();
-  map.panTo(new L.LatLng(pos[0], pos[1]));
-}
 
 jQuery(function () {
   init();
